bf7f66d327b63d7ea87b8368355edccc983529cf
Kanan Naghiyev <tu788lnmp@aztu.local>
2022-06-23 15:49:11 +0400

    new file

---
+++ practicatask1/Areas/Manage/Controllers/CoruselController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practicatask1.Areas.Manage.Controllers
+{
+    [Area("Manage")]
+    public class CoruselController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ practicatask1/DAL/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace practicatask1.DAL
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+    }
+}
